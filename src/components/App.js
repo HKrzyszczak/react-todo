8ed0dbf919e892d7e9445c3c3d0cd151efc5cf01
HKrzyszczak <h.krzyszczak@electriclight.pl>
2017-11-25 12:51:09 +0100

    add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 const App = () => {
     return (
-        <div>
-            cos
-        </div>
+        <Router>
+            <div>
+
+                <Route path='./' component={null} />
+                <Route path='./todo' component={null} />
+                <Route path='./counter' component={null} />
+            </div>
+        </Router>
     )
 };
-
-
 
 export default App;
